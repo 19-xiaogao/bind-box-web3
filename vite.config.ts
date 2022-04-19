c280a50713f65ca7770ffc6f472c921c33d2d20a
xiaolong <longjiuwei999@163.com>
2022-04-19 10:17:04 +0800

    configuration Item Settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import type { Alias } from "vite";
+
+function pathResolve(dir: string) {
+    return resolve(__dirname, ".", dir);
+}
+
+function createAlias(alias: [string, string][]): Alias[] {
+    return alias.map((item) => {
+        const [alia, src] = item;
+        return {
+            find: new RegExp(alia),
+            replacement: pathResolve(src) + "/",
+        };
+    });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    resolve: {
+        alias: createAlias([
+            ["@/", "src"],
+            ["@assets/", "src/assets"],
+            ["@comp/", "src/components"],
+            ["@api/", "src/api"],
+            ["@utils/", "src/utils"],
+            ["@types/", "src/types"],
+        ]),
+    },
+});
