64039277b31765e6414556bf9e409272684983fd
xiaolong <longjiuwei999@163.com>
2022-04-20 17:23:14 +0800

    write the query contract API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,93 @@
 import * as ethers from "ethers";
-import DRC751 from "./DRC721.json";
+import DRC751 from "./abi/DRC721.json";
+import DBOperation from "./abi/DBOperation.json";
+import { createAccessToken } from "@/api/initAdmin";
+import { encode, decode } from "js-base64";
+
+const DBchainConfig = {
+    appCode: "DYJGN3ZTBY",
+    netWork: "http://192.168.0.58:8545",
+};
 
 // DRC751 abi
-export const contractABI = DRC751.abi;
-
+const DRC751contractABI = DRC751;
 // DRC751 合约地址
-export const contractAddress = "0x33cc23476EBE1f8F2E0acA82546b1D22279B477F";
+const DRC751contractAddress = "0x5906De49949881C44D18325F65c9f45E12eBC47b";
 
-export function initDRC751Contract() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const signer = provider.getSigner();
-    const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-    return wavePortalContract;
+function getProvider() {
+    if (window.ethIsConnected) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        return signer;
+    }
+    return new ethers.providers.JsonRpcProvider(DBchainConfig.netWork);
 }
+
+export function initDRC721Contract() {
+    const provider = getProvider();
+    const Contract = new ethers.Contract(DRC751contractAddress, DRC751contractABI, provider);
+    return Contract;
+}
+export const DRC721Contract = initDRC721Contract();
+
+const DBOperationContractABI = DBOperation;
+const DBOperationContractAddress = "0x53F0b5B7D8E7B86fc79030e1F9048c2c5648F936";
+export function initDBOperationContract() {
+    const provider = getProvider();
+    const Contract = new ethers.Contract(DBOperationContractAddress, DBOperationContractABI, provider);
+    return Contract;
+}
+
+export const DBOperationContract = initDBOperationContract();
+
+export const queryBindBoxApi = async (offset: number, limit: number) => {
+    const queryParams: any = [
+        { method: "table", table: "contracts" },
+        { method: "offset", value: String(offset) },
+        { method: "limit", value: String(limit) },
+    ];
+    const accessToken = createAccessToken();
+    const encodeParams = encode(JSON.stringify(queryParams));
+    const result = await DBOperationContract.query_std_querier(
+        accessToken,
+        DBchainConfig.appCode,
+        encodeParams
+    );
+    const parseResult = JSON.parse(result);
+    return parseResult.map((item: any) => ({ ...item, desc: JSON.parse(decode(item.desc)) }));
+};
+
+export const queryBindBoxCountApi = async () => {
+    const queryParams: any = [{ method: "table", table: "contracts" }, { method: "count" }];
+    const accessToken = createAccessToken();
+    const encodeParams = encode(JSON.stringify(queryParams));
+    const result = await DBOperationContract.query_std_querier(
+        accessToken,
+        DBchainConfig.appCode,
+        encodeParams
+    );
+    console.log(result);
+    return JSON.parse(result);
+};
+
+export const queryBindBoxDetailApi = async (id: string) => {
+    const queryParams = [
+        { method: "table", table: "contracts" },
+        {
+            method: "where",
+            field: "id",
+            value: `${id}`,
+            operator: "=",
+        },
+    ];
+
+    const accessToken = createAccessToken();
+    const encodeParams = encode(JSON.stringify(queryParams));
+    const result = await DBOperationContract.query_std_querier(
+        accessToken,
+        DBchainConfig.appCode,
+        encodeParams
+    );
+    const parseResult = JSON.parse(result);
+    return parseResult.map((item: any) => ({ ...item, desc: JSON.parse(decode(item.desc)) }));
+};
---
+++ src/api/initAdmin.ts
@@ -0,0 +1,36 @@
+import bs58 from "bs58";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+
+const privKey = new Uint8Array([
+    29, 29, 223, 145, 133, 200, 122, 27, 31, 168, 151, 62, 91, 63, 97, 43, 172, 68, 101, 195, 127, 118, 184,
+    118, 109, 189, 12, 190, 129, 23, 54, 175,
+]);
+
+const pubKey = new Uint8Array([
+    3, 109, 131, 204, 159, 146, 9, 109, 32, 136, 146, 211, 155, 209, 237, 13, 112, 254, 245, 99, 114, 13, 132,
+    191, 206, 24, 188, 226, 33, 220, 191, 247, 107,
+]);
+
+function sign(bytes: any, privateKey: any) {
+    const hash = keccak256(new TextEncoder().encode(bytes));
+    const [signature] = secp.signSync(hash, privateKey, {
+        recovered: true,
+        der: false,
+    });
+    return signature;
+}
+
+function signForToken(str: any) {
+    var signature = sign(str, privKey);
+
+    var encodedPubKey = bs58.encode(pubKey);
+    var encodedSig = bs58.encode(signature);
+    var result = `${encodedPubKey}:${str}:${encodedSig}`;
+    return result;
+}
+export function createAccessToken() {
+    var time = "" + Date.now();
+    var result = signForToken(time);
+    return result;
+}
