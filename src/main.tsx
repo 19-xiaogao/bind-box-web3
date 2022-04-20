ceb87f95f23fc0cf650354f15cebc5b86f9e1423
xiaolong <longjiuwei999@163.com>
2022-04-20 18:07:35 +0800

    import initAdmin ts file

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'antd/dist/antd.css'
 import "./assets/css/index.scss"
+import "@/api/initAdmin"
 ReactDOM.render(
   <React.StrictMode>
     <App />
