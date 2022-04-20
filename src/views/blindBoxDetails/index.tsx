d7c56c65bd20aa52d5486d4d2813f11c30c6e007
xiaolong <longjiuwei999@163.com>
2022-04-20 18:06:46 +0800

    docking bind box detail pages

--- src/views/blindBoxDetails/index.tsx
+++ src/views/blindBoxDetails/index.tsx
@@ -1,19 +1,80 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { useSearchParams } from "react-router-dom"
 import HeaderJsx from '../../components/views/Header'
+import { queryBindBoxDetailApi } from "@/api/api"
+import { formatTime } from "@/utils"
 import "./bindBoxDetails.scss"
 
+const initValue = {
+    contract_address: "",
+    created_at: "",
+    id: "",
+    price: "",
+    release_number: "",
+    release_time: "",
+    tx_hash: "",
+    desc: {
+        desc: "",
+        name: "",
+        nft_metadatas: [{
+            image: ""
+        }]
+    }
+}
+const reducer = (state = initValue, action: any) => {
+    switch (action.type) {
+        case "change":
+            return { ...state, ...action.payload };
+        default:
+            return state;
+    }
+}
+
+
 function BindBoxDetailJsx() {
+    const [searchParams] = useSearchParams()
+    const [bindDetailBox, dispatchBindDetailBox] = useReducer(reducer, initValue);
+
+    const queryBindBoxDetail = async () => {
+        const id = searchParams.get('id') as string
+        const result = await queryBindBoxDetailApi(id)
+        console.log(result);
+
+        dispatchBindDetailBox({ type: "change", payload: result[0] })
+
+
+    }
+    useEffect(() => {
+        queryBindBoxDetail()
+    }, [])
+
+
+    const renderNftList = () => {
+        return bindDetailBox.desc.nft_metadatas.slice(1).map((item: any, key: number) => (<div className='box' key={key}>
+            <div className='nft'>
+                <img src={item.image} alt="" />
+            </div>
+            <div className='nft-decs'>
+                <div>{item.name}</div>
+                <div>{item.attributes[0].level}</div>
+                {/* <div>发行数量 : {item.name} </div> */}
+                <div>概率 : {item.attributes[0].probability}</div>
+            </div>
+        </div>))
+    }
+
     return <div className='root-page'>
         <HeaderJsx />
         <main>
             <div className='bbd-product'>
                 <div className='box-show'>
-                    <img src="https://public.nftstatic.com/static/nft/zipped/57a49901aebb469899f59e26984ca64d_zipped.gif" alt="" />
+                    <img src={bindDetailBox.desc.nft_metadatas[0].image} alt="" />
                 </div>
                 <div className='box-content'>
-                    <p className='box-name'>Galaxy Blitz NFT Planets
+                    <p className='box-name'>
+                        {bindDetailBox.desc.name}
                     </p>
-                    <div className='box-source'>
+                    {/* <div className='box-source'>
                         <div className='publisher'>
                             <div className='p-owner'>
                                 <img src="https://public.nftstatic.com/static/nft/zipped/cf66bd860fb147199032c4711a75d3c7_zipped.jpeg" />
@@ -32,15 +93,15 @@ function BindBoxDetailJsx() {
                                 <div>GalaxyBlitz</div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                     <div className='box-price-message'>
                         <div className='box-price'>
                             <div>当前出价</div>
-                            <div>180 DUSD</div>
-                            <div>≈ $ 179.98 </div>
+                            <div>{bindDetailBox.price}DBC</div>
+                            {/* <div>≈ $ 179.98 </div> */}
                         </div>
                         <div className='box-end-time'>
-                            <div className='time-title'>拍卖结束时间</div>
+                            <div className='time-title'>拍卖开启时间</div>
                             <div className='time-show'>
                                 <div className='day'>
                                     <span>00</span>
@@ -69,94 +130,29 @@ function BindBoxDetailJsx() {
                 <div className='decs-details'>
                     <div className='price'>
                         <span>发行价</span>
-                        <span>88 BUSD</span>
+                        <span>{bindDetailBox.price}DBC</span>
                     </div>
                     <div className='count'>
                         <span>发行数量</span>
-                        <span>2,000</span>
+                        <span>{bindDetailBox.release_number}</span>
                     </div>
                     <div className='time'>
                         <span>发布日期</span>
-                        <span>2022-04-13 </span>
+                        <span>{formatTime(bindDetailBox.release_time)} </span>
                     </div>
                     <div className='contractAddress'>
                         <span>合约地址</span>
-                        <span>0x9390372cd986df48b383d7dc95c3de53fecebe74 </span>
+                        <span>{bindDetailBox.contract_address} </span>
                     </div>
                 </div>
                 <p className='decs'>
-                    X World Games is the next-generation decentralized gaming metaverse. Two blockchain games, Dream Card and Dream Card V2 have been launched. Dream Card V2 is a multi-player RPG game built with Unity and features in PvE mode. This issue adds of new hero, Anubis, and a powerful returning player favourite, Andal. Both our new Heroes come with direct access to Dream Card v1 & v2. Anubis will make her first debut as Mage in Dream Card V2!
+                    {bindDetailBox.desc.desc}
                 </p>
             </div>
             <div className='rarity-details'>
                 <h2>稀有度详情</h2>
                 <div className='rarity-box'>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
-                    <div className='box'>
-                        <div className='nft'>
-                            <img src="https://public.nftstatic.com/static/nft/zipped/6fa432f7c803473d8559d78381b03b85_zipped.gif" alt="" />
-                        </div>
-                        <div className='nft-decs'>
-                            <div>Anubis-Myth</div>
-                            <div>SSR</div>
-                            <div>发行数量 : 88</div>
-                            <div>概率 : 3.30%</div>
-                        </div>
-                    </div>
+                    {renderNftList()}
                 </div>
             </div>
         </main>
