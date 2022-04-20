ca98d208dbe15269b60c1f3194322aab9a281f25
xiaolong <longjiuwei999@163.com>
2022-04-20 18:03:42 +0800

    global setting is connect metamask

--- src/components/views/Header/index.tsx
+++ src/components/views/Header/index.tsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useMemo, useCallback } from "react"
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { IMetamaskErrResponse } from "@/types/metamask"
+import { notificationInfo, notificationSuccess } from "@/utils/index"
 import "./header.scss"
-import * as ethers from "ethers"
-import abi from "../../../abi/DRC721.json";
 
 const HeaderJsx = () => {
 
     const [currentAccount, setCurrentAccount] = useState("")
-    const contractAddress = "0x33cc23476EBE1f8F2E0acA82546b1D22279B477F";
-
-    const contractABI: any = abi.abi;
+    const navigate = useNavigate();
 
     const handleConnectMeatMask = async () => {
-        const result = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const { ethereum } = window
+        if (!ethereum) return notificationInfo("您没有安装小狐狸钱包,请先去安装。")
+        try {
+            const result = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setCurrentAccount(result)
+            notificationSuccess("您同意了网站授权。")
+        } catch (error: IMetamaskErrResponse | any) {
+            if (error.code === 4001) {
+                notificationInfo("您拒绝了网站授权。")
+            }
+        }
     }
 
     const checkIfWalletIsConnected = async () => {
@@ -20,75 +29,40 @@ const HeaderJsx = () => {
             const { ethereum } = window;
 
             if (!ethereum) {
-                console.log("Make sure you have metamask!");
                 return;
-            } else {
-                console.log("We have the ethereum object", ethereum);
             }
-
-            const accounts = await ethereum.request({ method: "eth_accounts" });
-
+            const accounts = await window.ethereum.request({ method: "eth_accounts" });
             if (accounts.length !== 0) {
                 const account = accounts[0];
-                console.log("Found an authorized account:", account);
                 setCurrentAccount(account)
-            } else {
-                console.log("No authorized account found")
+                window.ethIsConnected = true;
             }
         } catch (error) {
+            window.ethIsConnected = false;
             console.log(error);
         }
     }
 
 
-    useEffect(() => {
-        checkIfWalletIsConnected();
-        init()
-    }, [])
-
-    async function init() {
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-        console.log(ethers.utils.parseEther(String(1)));
-
-        const tx = await wavePortalContract["buyTickets"](1, {
-            gasPrice: 1000000000,
-            gasLimit: 300000,
-            value: ethers.utils.parseEther(String(1))
-        })
-        const transData = await tx.wait()
-
-        // const buyTickets = await wavePortalContract.buyTickets(1);
-        // await buyTickets.wait();
-
-        // const params = {
-        //     from: "0xa8433e531575CCCf9E19978800D452807C5E3F64",
-        //     to: contractAddress,
-        //     // value: '0x1',
-        //     // gasPrice: '0x1',
-        //     // gas: '0x1',
-        //     data:
-        //         '0x2f3666370000000000000000000000000000000000000000000000000000000000000001',
-        // }
-        // console.log(window.ethereum);
-
-        // window.ethereum
-        //     .request({
-        //         method: 'eth_sendTransaction',
-        //         params: [
-        //             params
-        //         ],
-        //     })
-        //     .then((result: any) => {
-        //     })
-        //     .catch((error: any) => {
-        //     });
+    const handleAvatarClick = () => {
+        navigate("/assets");
     }
 
+    //TODO: 优化， 每次都会执行
+    const renderHeaderRightBox = useCallback(() => {
+        if (!currentAccount) {
+            return <div className="h-connect">
+                <span onClick={handleConnectMeatMask}>连接MetaMask</span>
+            </div>
+        }
+        return <div className="h-avatar" onClick={handleAvatarClick}>
+            <img src="https://storage.googleapis.com/opensea-static/opensea-profile/22.png" alt="" />
+        </div>
+    }, [currentAccount])
 
+    useEffect(() => {
+        checkIfWalletIsConnected()
+    }, [])
 
     return <header className="h-header">
         <div className="h-logo">
@@ -97,9 +71,7 @@ const HeaderJsx = () => {
             </Link>
 
         </div>
-        <div className="h-connect">
-            <span onClick={handleConnectMeatMask}>连接MetaMask</span>
-        </div>
+        {renderHeaderRightBox()}
     </header>
 }
 
