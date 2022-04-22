168bbc5dddadb375543faf9bfac5ffd48de930c3
xiaolong <longjiuwei999@163.com>
2022-04-22 18:43:53 +0800

    add Listen accounts Changed functon

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "../views/Home"
 import AllBindBox from '../views/allBIndBox'
@@ -6,6 +6,15 @@ import BindBoxDetailJsx from '../views/blindBoxDetails'
 import PersonAssetJsx from "../views/personalAssets"
 import OpenBindBoxJsx from '../views/openBIndBox'
 const RouterJsx = () => {
+    useEffect(() => {
+        // 监听账户发生变化
+        window.ethereum.on('accountsChanged', (accounts: string[]) => {
+            if (window.location.pathname == '/assets') {
+                return window.location.href = "/"
+            }
+            window.location.reload();
+        });
+    }, [])
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
