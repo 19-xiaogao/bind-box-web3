b8012b4ec77c73ab247dcad2efa79f4184030794
xiaolong <longjiuwei999@163.com>
2022-04-24 17:52:49 +0800

    remote useState function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import React from "react";
 import RouterJsx from "./router";
 function App() {
-    const [count, setCount] = useState();
-
     return <RouterJsx />;
 }
 
