a32ca23ae05ff2519d3550505552d5aacac40926
xiaolong <longjiuwei999@163.com>
2022-05-09 10:01:54 +0800

    add lazy components

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
-import AllBindBox from "../views/allBIndBox";
+// import AllBindBox from "../views/allBIndBox";
+
+const AllBindBox = React.lazy(() => import("../views/allBIndBox"));
 import BindBoxDetailJsx from "../views/blindBoxDetails";
 import PersonAssetJsx from "../views/personalAssets";
 import OpenBindBoxJsx from "../views/openBIndBox";
 import NftDetailJsx from "@/views/nftDetails";
 import ContextJSX from "@/components/hooks/globalContent"
+
+
+interface SuspenseComponentI {
+    children: React.LazyExoticComponent<() => JSX.Element>
+}
+
+const SuspenseComponent = (props: any) => {
+    return <React.Suspense fallback={<>...</>}>
+        {props.children}
+    </React.Suspense>
+}
+
+
 const RouterJsx = () => {
     useEffect(() => {
         // 监听账户发生变化
@@ -20,7 +35,11 @@ const RouterJsx = () => {
             <ContextJSX>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/allBindBox" element={<AllBindBox />} />
+                    <Route path="/allBindBox" element={
+                        <SuspenseComponent>
+                            <AllBindBox />
+                        </SuspenseComponent>
+                    } />
                     <Route path="/bindBoxDetails" element={<BindBoxDetailJsx />} />
                     <Route path="/assets" element={<PersonAssetJsx />} />
                     <Route path="/openBindBox" element={<OpenBindBoxJsx />} />
