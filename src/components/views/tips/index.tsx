f7952977c2089df23dc4b8cfbe3b14363a506a25
xiaolong <longjiuwei999@163.com>
2022-05-12 15:15:45 +0800

    add pc tips message

--- src/components/views/tips/index.tsx
+++ src/components/views/tips/index.tsx
@@ -3,9 +3,11 @@ import { Modal } from 'antd'
 
 const TipsJsx: React.FC<any> = (props) => {
     const [visible, setVisible] = useState<boolean>(false)
+
+    const [isH5, setIsH5] = useState<boolean>(false)
     useEffect(() => {
         const { ethereum } = window
-
+        setIsH5(h5judge())
         const tips = localStorage.getItem('tips')
         if (tips) {
             setVisible(false)
@@ -18,6 +20,20 @@ const TipsJsx: React.FC<any> = (props) => {
         }
 
     }, [])
+    function h5judge() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = ["Android", "iPhone",
+            "SymbianOS", "Windows Phone",
+            "iPad", "iPod"];
+        var flag = false;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = true;
+                break;
+            }
+        }
+        return flag
+    }
 
     const handleCloseClick = () => {
         setVisible(false)
@@ -25,7 +41,12 @@ const TipsJsx: React.FC<any> = (props) => {
     }
 
     return <Modal visible={visible} title="提示" closable={false} footer={null} centered onCancel={handleCloseClick}>
-        <p>你好，本网站需要你安装MeatMask钱包，进行消息授权，<a href="https://metamask.io/" target="_blank" >安装地址。</a></p>
+        {
+            isH5
+                ? <p>你好，请在pc端打开该网页。</p>
+                : <p>你好，本网站需要你安装MeatMask钱包，进行消息授权，<a href="https://metamask.io/" target="_blank" >安装地址。</a></p>
+        }
+
     </Modal>
 }
 
